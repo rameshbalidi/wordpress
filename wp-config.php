cb384c7dd1ac5898779f89cea43a99c39d11507f
rameshbalidi <66063936+rameshbalidi@users.noreply.github.com>
2020-07-10 20:06:25 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'rameshdb' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'ramesh' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'ncpl@1234' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
